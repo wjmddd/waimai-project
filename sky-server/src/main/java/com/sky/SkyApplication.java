12d3edb229386d03dcbf42e4a224070b0eae8308
王嘉冕 <11642938+wjmddd@user.noreply.gitee.com>
2024-12-19 14:04:30 +0800

    缓存套餐

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -5,12 +5,14 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 @MapperScan("com.sky.mapper")
+@EnableCaching
 public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
